4fb794265a3a0af82ea73f51c1b0158008c9803c
Daniel Potter <daniel.potter@onealphatech.com>
2023-01-02 22:59:41 +1100

    Initial commit with work done migrating my site to Blazor.

---
+++ Shared/Partials/NavMenu.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace DeejpotterBlazorWasm.Shared.Partials;
+
+public partial class NavMenu
+{
+    private bool _collapseNavMenu = true;
+
+    private string? NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
+
+    private void ToggleNavMenu()
+    {
+        _collapseNavMenu = !_collapseNavMenu;
+    }
+}
